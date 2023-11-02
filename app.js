ba88de659452f333664935fc613b66a00fd9d6a9
Vinay89824 <s565587@nwmissouri.edu>
2023-11-01 23:26:34 -0500

    created

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var gadgetRouter = require('./routes/gadget');
+var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 var app = express();
 
 // view engine setup
@@ -21,7 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/gadget', gadgetRouter);
+app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
